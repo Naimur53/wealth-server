87e218b7355c46f153cd178b21080db7bb014755
naimur53 <naimurrhman53@gmail.com>
2024-02-24 08:57:35 +0600

    added createAt

--- src/app/modules/location/location.validation.ts
+++ src/app/modules/location/location.validation.ts
@@ -3,11 +3,13 @@ import { z } from 'zod';
 const createValidation = z.object({
   body: z.object({
     name: z.string({ required_error: 'Location name is required' }),
+    imgUrl: z.string({ required_error: 'ImgUrl name is required' }),
   }),
 });
 const updateValidation = z.object({
   body: z.object({
     name: z.string({ required_error: 'Location name is required' }),
+    imgUrl: z.string({ required_error: 'imgUrl  is required' }).optional(),
   }),
 });
 const getSingleLocation = z.object({
