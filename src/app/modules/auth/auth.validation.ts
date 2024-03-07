524b154b00695039a9db2f3b393e4b2801e96e22
naimur53 <naimurrhman53@gmail.com>
2024-03-07 23:08:10 +0600

    doe

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -12,7 +12,7 @@ const createAuthZodSchema = z.object({
 });
 const loginZodSchema = z.object({
   body: z.object({
-    email: z.string({ required_error: 'Email is required' }),
+    email: z.string({ required_error: 'Email is required!' }),
     password: z.string({ required_error: 'Password is required' }),
   }),
 });
